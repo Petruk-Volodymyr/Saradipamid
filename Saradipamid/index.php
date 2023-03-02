6cae29ea8ecf5e305077b0054935bd642c2132ad
Petruk-Volodymyr <126797257+Petruk-Volodymyr@users.noreply.github.com>
2023-03-02 16:40:59 +0100

    Saradipamid
    Strona dla sprzedaży skarpetek

---
+++ Saradipamid/index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+	<meta charset="UTF-8">
+	<title>Saradipamid</title>
+	<link rel="stylesheet" href="style.css">
+	
+</head>
+<body>
+	<div class="lal">
+		<div class="buba" onclick="of()">
+
+			<div class="beba">
+				
+				<iframe src="https://www.youtube.com/embed/88cvrY82yVU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+
+			</div>
+		</div>
+	</div>
+	<script>
+		function on() {
+			document.querySelector('.lal').style.display = 'block';
+		}
+		function of() {
+			document.querySelector('.lal').style.display = 'none';
+		}
+
+	</script>
+	<header>
+		
+		<div class="menu menu1">
+			
+			<p><a style="font-size: 30px; text-decoration: none;" href="index.php">Saradipamid</a></p>
+
+		</div>
+		<div class="menu menu2">
+			
+			<a href="#otz">Komentarze</a>
+			<a href="#towar">Towary</a>
+			<a href="#stop">O nas</a>
+
+		</div>
+		<div class="menu menu3"></div>
+
+	</header>
+	<br>
+	<article id="witr">
+		
+		<section>
+			
+			<div>
+				
+				<div>
+					
+					<h1 style=" color: #433D29;font-size: 30px;">Bardzo dorbe skarpetki.<br>Kupuj teraz i będzie ci dobrze.</h1>
+					<br>
+					<p style="color: #433D29;">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic quae dolorem, commodi. Recusandae iusto, temporibus est quia, labore, enim voluptatem facere nostrum dicta quos laborum laudantium, maiores doloremque natus odit.
+					Earum tempore officiis, ratione temporibus omnis accusantium natus, iure assumenda ipsum ducimus iusto perspiciatis id quaerat, esse placeat qui voluptate, sunt ullam. Ullam deleniti, adipisci incidunt odit eaque, repudiandae eligendi!</p>
+					<br>
+					<button class="knopka1" onclick="on()">
+						► Ogarni filmik
+					</button>
+					<button class="knopka2">
+						Więcej informacji
+					</button>
+
+				</div>
+
+			</div>
+
+		</section>
+
+	</article>
+	<br>
+	<article id="otz">
+		
+		<div class="koment koment1">
+			<h1>
+				WorekSpermy
+			</h1>
+			<br>
+			<p>
+				hack because lose, unskilled without
+			</p>
+			<img src="" alt="">
+		</div>
+		<div class="koment koment2">
+			<h1>
+				Sensey
+			</h1>
+			<br>
+			<p>
+				Cholera, Atak Tytanów to arcydzieło
+			</p>
+			<img src="" alt="">
+		</div>
+		<div class="koment koment3">
+			<h1>
+				Natural
+			</h1>
+			<br>
+			<p>
+				Za komunizm i dwor. Ale ja amerykańczyk
+			</p>
+			<img src="" alt="">
+		</div>
+		<div class="koment koment4">
+			<h1>
+				Blanche
+			</h1>
+			<br>
+			<p>
+				Ja pidaras
+			</p>
+			<img src="" alt="">
+		</div>
+		<div class="koment koment5">
+			<h1 style="color: #EFF5F9;">
+				Żenia
+			</h1>
+			<br>
+			<p style="color: #EFF5F9;">
+				Ja brudny pizdabol
+			</p>
+			<img src="" alt="">
+		</div>
+
+	</article>
+	<br>
+	<article id="towar">
+		
+		<?php 
+
+		$conn = mysqli_connect('localhost', 'root', '', 'saradipamid');
+		$com = "SELECT noski.id, noski.nazwa, noski.cena, noski.foto, typy.typ, typy.opis FROM noski, typy WHERE noski.typ = typy.typ; ";
+		$casd = mysqli_query($conn, $com);
+		
+		while ($row = mysqli_fetch_assoc($casd)) {
+
+			echo 
+			"
+				<div class='tow'>
+					
+					<img src='".$row['foto']."'>
+					<div>
+						<h1 style='font-size: 30px;'>".$row['nazwa']."</h1>
+						<p>Typ:".$row['typ']."</p>
+						<p>Cena:".$row['cena']."$</p>
+						<a href='insert.php?id=".$row['id']."'><input type='submit' value='Zamów'></a>
+					</div>
+				</div>
+			"
+			;
+		}
+
+		?>
+
+
+
+	</article>
+	<footer id="stop">
+		
+		<div class="fot1">
+			
+			<h1>O nas</h1>
+			<br>
+			<p>Jesteśmy firmą z Karpat. Produkujemy najlepsze skarpetki na całą Urkainę. Nasze skarpetki mogą wytrzymać bombę, i nawet agresywnego Żiomka który pomyślał że ty dziura dla seksu.</p>
+			<br>
+			<h2>Kontakt:</h2><a href="#">skarpetkikarpaty@gmail.com</a>
+
+		</div>
+		<div class="fot2">
+			
+			<h1>Wsparcie</h1>
+			<br>
+			<p>Jak masz jakiś problem to możesz napisać do naszego specjalisty <a href="https://www.youtube.com/shorts/CbcFFYhYMfg" target="blank">Wiktora</a>, który ci pomoże, ale jak powiesz coś złego. To ciebie gówniaka wyżucimy do kanały.</p>
+			<br>
+			<h2>Cpyright&copy Nosky Karpaty Interprice 2023</h2>
+
+		</div>
+
+	</footer>
+
+</body>
+</html>
---
+++ Saradipamid/insert.php
@@ -0,0 +1,112 @@
+<?php 
+
+		$conn = mysqli_connect('localhost', 'root', '', 'saradipamid');
+		$ide = $_GET['id'];
+		$poc = "SELECT * FROM noski, typy WHERE noski.id=$ide and noski.typ = typy.typ;";
+		$opo = mysqli_query($conn, $poc);
+		$row = mysqli_fetch_assoc($opo);
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="styl_inse.css">
+</head>
+<body>
+		<header>
+		
+		<div class="menu menu1">
+			
+			<p><a style="font-size: 30px; text-decoration: none;" href="index.php">Saradipamid</a></p>
+
+		</div>
+		<div class="menu menu2">
+			
+			<a href="#stop">O nas</a>
+
+		</div>
+		<div class="menu menu3"></div>
+
+	</header>
+	<hr>
+	<?php
+			// 	$typulaka = $row['typ'];
+			// if ($typulaka == 1) {
+			// 	$row['typ'] = "Wełniane";
+			// }else if ($typulaka == 2) {
+			// 	$row['typ'] = "Bawełniane";
+			// }else if ($typulaka == 3) {
+			// 	$row['typ'] = "Syntetyczne";
+			// }else if ($typulaka == 4) {
+			// 	$row['typ'] = "Specjalistyczne";
+			// }
+
+	echo "		 <article>
+	 	
+	 	<div class='type1'>
+	 		
+	 		<img src='".$row['foto']."' alt=''>
+
+	 	</div>
+	 	<div class='type2'>
+	 		<div class='oczko'>
+	 		<h1 style='text-align: center;'>".$row['nazwa']."</h1><br>
+		 		<div class='typ'>Typ:".$row['typ']."</div>
+		 		<div class='cena'>Cena:".$row['cena']."$</div>
+		 		<div class='opis'>
+		 			
+		 			<p>
+		 				
+		 				".$row['opis']."
+
+		 			</p>
+
+		 		</div>
+		 		<br>
+		 		<br>
+		 		<br>
+		 		<br>
+		 		<p><input type='submit' value='Zamów' style='cursor: pointer;'></p>
+			</div>
+
+	 	</div>
+
+	 </article>	";
+
+	 ?>
+
+
+	 <br>
+	<footer id="stop">
+			
+			<div class="fot1">
+				
+				<h1>O nas</h1>
+				<br>
+				<p>Jesteśmy firmą z Karpat. Produkujemy najlepsze skarpetki na całą Urkainę. Nasze skarpetki mogą wytrzymać bombę, i nawet agresywnego Żiomka który pomyślał że ty dziura dla seksu.</p>
+				<br>
+				<h2>Kontakt:</h2><a href="#">skarpetkikarpaty@gmail.com</a>
+
+			</div>
+			<div class="fot2">
+				
+				<h1>Wsparcie</h1>
+				<br>
+				<p>Jak masz jakiś problem to możesz napisać do naszego specjalisty <a href="https://www.youtube.com/shorts/CbcFFYhYMfg" target="blank">Wiktora</a>, który ci pomoże, ale jak powiesz coś złego. To ciebie gówniaka wyżucimy do kanały.</p>
+				<br>
+				<h2>Cpyright&copy Nosky Karpaty Interprice 2023</h2>
+
+			</div>
+
+	</footer>
+</body>
+</html>
+<?php 
+
+ mysqli_close($conn)
+
+ ?>
